13972fa2f899f9203002243bf00f61c248580273
manavkhadka0 <manavkhadka0@gmail.com>
2025-01-03 16:11:24 +0545

    dasd

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,7 +1,7 @@
 import EmailTemplate from "@/components/email-template";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API);
 
 interface FormData {
   name?: string;
